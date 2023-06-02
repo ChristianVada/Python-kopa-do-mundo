d564365a2c24aa917c615e4cc274fe90f338e360
Christian <christianryuji@hotmail.com>
2023-06-01 23:25:12 -0300

    feat: task 3

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from .models import Team
+from django.forms.models import model_to_dict
 
-# Create your views here.
+from utils import data_processing
+from exceptions import *
+
+
+class TeamView(APIView):
+    def get(self, request: Request) -> Response:
+        teams = Team.objects.all()
+
+        teams_list = []
+        for team in teams:
+            team_dict = model_to_dict(team)
+            teams_list.append(team_dict)
+
+        return Response(teams_list, status.HTTP_200_OK)
+
+    def post(self, request: Request) -> Response:
+        try:
+            data_processing(request.data)
+            team = Team.objects.create(**request.data)
+            team_dict = model_to_dict(team)
+
+            return Response(team_dict, status.HTTP_201_CREATED)
+
+        except NegativeTitlesError:
+            return Response(
+                {"error": "titles cannot be negative"}, status.HTTP_400_BAD_REQUEST
+            )
+
+        except InvalidYearCupError:
+            return Response(
+                {"error": "there was no world cup this year"},
+                status.HTTP_400_BAD_REQUEST,
+            )
+
+        except ImpossibleTitlesError:
+            return Response(
+                {"error": "impossible to have more titles than disputed cups"},
+                status.HTTP_400_BAD_REQUEST,
+            )
